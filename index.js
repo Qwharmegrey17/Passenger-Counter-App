94adedb8aecaf56573c787d16100ccf2eec67119
Eli Grey-Gonyoe <eligray2001@outlook.com>
2026-05-21 07:56:36 +0000

    Initial commit: start Javascript learning project

---
+++ index.js
@@ -0,0 +1,19 @@
+// Write js variables in camelCase (myVariableName)
+let countEl = document.getElementById("count-el") // pass in arguments (input) into the function, like getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+console.log(countEl) // passing in the variable to log
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    console.log(count)
+}
+
