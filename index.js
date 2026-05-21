8642174ead35ba17d030b7213f185f76a4ac43b6
Qwharmegrey17 <eligray2001@outlook.com>
2026-05-21 12:01:05 +0000

    "final final changes and edits"

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 // Write js variables in camelCase (myVariableName)
-let countEl = document.getElementById("count-el") // pass in arguments (input) into the function, like getElementById("count-el")
-let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el"); // pass in arguments (input) into the function, like getElementById("count-el")
+let saveEl = document.getElementById("save-el");
 
-console.log(countEl) // passing in the variable to log
+console.log(countEl); // passing in the variable to log
 
-let count = 0
+let count = 0;
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+  count = count + 1;
+  countEl.innerText = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
-
